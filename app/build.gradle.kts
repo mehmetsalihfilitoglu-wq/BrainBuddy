4bdca24fdf7352f36aac39587eef250c54be85c7
Mehmet <mehmet@example.com>
2026-03-02 17:15:38 +0100

    Add new activities to AndroidManifest and update dependencies
    - Added ScreenTimeActivity, TestSettingsActivity, and SecurityActivity to the AndroidManifest.
    - Included androidx.gridlayout dependency in build.gradle.kts for layout support.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
